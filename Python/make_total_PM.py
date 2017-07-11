a8d0ffb46bc53d663268105bc2d1a23b4bdbafa3
stevenjoelbrey <sbrey@ozone.atmos.colostate.edu>
2017-07-11 15:32:08 -0600

    estimating total Surface layer PM25 mass updated with new equation and file parth structures.

--- Python/make_total_PM.py
+++ Python/make_total_PM.py
@@ -5,8 +5,8 @@
 ###############################################################################
 # This script will be used to calculate total PM concentration. 
 # From Lamarque et al 2012
-# Please refer to the README_CESM and the recomended publications for a better
-# understanding of the formula in this script. 
+# Please refer to the Make_PM25_README in /FIRE_EMissions and the recomended 
+# publications for a better understanding of the formula in this script. 
 
 import sys
 print 'Number of arguments:', len(sys.argv), 'arguments.'
@@ -18,7 +18,7 @@ if len(sys.argv) != 1:
 
 else:
 	print 'Using default '
-	scenario    = '2000Base'
+	scenario    = '2000Firev1'
 
 import os
 import numpy as np
@@ -26,10 +26,14 @@ from netCDF4 import Dataset
 import matplotlib.pyplot as plt
 import cesm_nc_manager as cnm
 import time as timer
+import os.path
+
+dataDirBase= os.path.join("/pierce-scratch","mariavm")
 
 startTime = timer.time()
 
 # species| Formula                       | emitted_by_fire_module
+#--------------------------------------------------------------------
 # CB1    | C, hydrophpbic black carbon   | y, as BC
 # CB2    | C, hydrophilic black carbon   | y, as BC
 # DST01  | AlSiO5                        | n
@@ -47,90 +51,89 @@ startTime = timer.time()
 # SSLT03 | NaCl                          | n
 # SSLT04 | NaCl                          | n
 
-# To calculate PM2.5,
-#		BC = CB1 + CB2
-#		SOA = SOAB + SOAI + SOAM + SOAX + SOAT
-#		FineSSLT = SSLT01 + SSLT02
-#		FineDST = DST01 + DST02
-#		Organic aerosol (OA) = OC1 + OC2 + SOA
-	
-#		PM25 = BC + 1.2*OA + SO4 + NH3NO4 + FineSSLT + FineDST
+# To calculate PM2.5 = 
+# 	  SO4 + 
+#         CB1 + CB2 + 
+#	  1.8*(OC1 + OC2) + 
+#	  (SOAT + SOAM + SOAI + SOAB + SOAX) +
+#         SSLT01 + SSLT02
+#         NH4NO3 + 
+#         DST01 + DST02 + 
 
 
-def getSelf(scenario, species):
+def getSelf(dataDirBase, scenario, NCVariable):
 	"""Quick function for returning nc file connection."""
-	ncFile = cnm.makeAQNCFile(species, scenario, 'daily')
+	ncFile = cnm.makeAQNCFile(dataDirBase, NCVariable, scenario, tStep="daily")
 	nc     = Dataset(ncFile, 'r')
-	ncVar  = nc.variables[species]
+	ncVar  = nc.variables[NCVariable]
 	return ncVar 
+
+# Get sulfate
+SO4_SRF = getSelf(dataDirBase, scenario, 'SO4_SRF')
 
 # Get black carbon
 # BC = CB1 + CB2
-CB1_SRF = getSelf(scenario, 'CB1_SRF')
-CB2_SRF = getSelf(scenario, 'CB2_SRF')
+CB1_SRF = getSelf(dataDirBase, scenario, 'CB1_SRF')
+CB2_SRF = getSelf(dataDirBase, scenario, 'CB2_SRF')
 
 # Get secondary organic aerosol (SOA)
 # SOA = SOAB + SOAI + SOAM + SOAX + SOAT
-SOAB_SRF = getSelf(scenario, 'SOAB_SRF')
-SOAI_SRF = getSelf(scenario, 'SOAI_SRF')
-SOAM_SRF = getSelf(scenario, 'SOAM_SRF')
-SOAX_SRF = getSelf(scenario, 'SOAX_SRF')
-SOAT_SRF = getSelf(scenario, 'SOAT_SRF')
+SOAB_SRF = getSelf(dataDirBase, scenario, 'SOAB_SRF')
+SOAI_SRF = getSelf(dataDirBase, scenario, 'SOAI_SRF')
+SOAM_SRF = getSelf(dataDirBase, scenario, 'SOAM_SRF')
+SOAX_SRF = getSelf(dataDirBase, scenario, 'SOAX_SRF')
+SOAT_SRF = getSelf(dataDirBase, scenario, 'SOAT_SRF')
 
 # Get sea salt aerosol
 # FineSSLT = SSLT01 + SSLT02
-SSLT01_SRF = getSelf(scenario, 'SSLT01_SRF')
-SSLT02_SRF = getSelf(scenario, 'SSLT02_SRF')
+SSLT01_SRF = getSelf(dataDirBase, scenario, 'SSLT01_SRF')
+SSLT02_SRF = getSelf(dataDirBase, scenario, 'SSLT02_SRF')
 
 # Get dust
 # FineDST = DST01 + DST02
-DST01_SRF = getSelf(scenario, 'DST01_SRF')
-DST02_SRF = getSelf(scenario, 'DST02_SRF')
+DST01_SRF = getSelf(dataDirBase, scenario, 'DST01_SRF')
+DST02_SRF = getSelf(dataDirBase, scenario, 'DST02_SRF')
 
 # Organic Aerosol (OA)
-# Organic aerosol (OA) = OC1+OC2+SOA
-OC1_SRF = getSelf(scenario, 'OC1_SRF')
-OC2_SRF = getSelf(scenario, 'OC2_SRF')
-
-# Get sulfate
-SO4_SRF = getSelf(scenario, 'SO4_SRF')
+# Organic aerosol (OA) = OC1+OC2
+OC1_SRF = getSelf(dataDirBase, scenario, 'OC1_SRF')
+OC2_SRF = getSelf(dataDirBase, scenario, 'OC2_SRF')
 
 # Get NH4NO3 (Ammonium nitrate)
-# NOTE: There is a typo in the README_CESM. NH4NO3 is written NH3NO4
-NH4NO3_SRF = getSelf(scenario, 'NH4NO3_SRF')
+NH4NO3_SRF = getSelf(dataDirBase, scenario, 'NH4NO3_SRF')
 
 # All needed nc connections are made. Now do the math for total PM2.5
-t   = getSelf(scenario, 'date')
+t   = getSelf(dataDirBase, scenario, 'date')
 n_t = len(t)
 
 # Make an array to store the PM2.5
 PM25     = np.zeros(SO4_SRF.shape)
 FirePM25 = np.zeros(SO4_SRF.shape)
 
-for i in range(n_t): #n_t
+for i in range(n_t): 
 
-	#print i
-
-	BC  = CB1_SRF[i,:,:] + CB2_SRF[i,:,:]
-
-	FineSSLT = SSLT01_SRF[i,:,:] + SSLT02_SRF[i,:,:]
-
-	FineDST = DST01_SRF[i,:,:] + DST02_SRF[i,:,:]
-	
-	SOA = SOAB_SRF[i,:,:] + SOAI_SRF[i,:,:] + SOAM_SRF[i,:,:]+\
-          SOAX_SRF[i,:,:] + SOAT_SRF[i,:,:]
-
-	OA = OC1_SRF[i,:,:] + OC2_SRF[i,:,:] + SOA
+	#print 'Percent Complete:' + str(i/float(n_t) * 100.)
 
 	SO4 = SO4_SRF[i,:,:]
 
+	BC  = CB1_SRF[i,:,:] + CB2_SRF[i,:,:]
+
+	OC = OC1_SRF[i,:,:] + OC2_SRF[i,:,:] 
+
+	SOA = SOAT_SRF[i,:,:] + SOAM_SRF[i,:,:] +  SOAI_SRF[i,:,:] +\
+              SOAB_SRF[i,:,:] + SOAX_SRF[i,:,:] 
+
 	NH4NO3 = NH4NO3_SRF[i,:,:]
 
+	FineDST = DST01_SRF[i,:,:] + DST02_SRF[i,:,:]
+
+	FineSSLT = SSLT01_SRF[i,:,:] + SSLT02_SRF[i,:,:]
+
 	# Calculate PM2.5 in units of kg/kg (all of these arrays are kg/kg)
-	PM25[i,:,:] = BC + 1.2 * OA + SO4 + NH4NO3 + FineSSLT + FineDST
+	PM25[i,:,:] = SO4 + BC + 1.8*OC + SOA + NH4NO3 + FineDST + FineSSLT
 
 	# Calculate PM2.5 emitted directly by fires
-	FirePM25[i,:,:] = BC + OC1_SRF[i,:,:] + OC2_SRF[i,:,:]
+	FirePM25[i,:,:] = BC + OC
 	
 
 #print 'Working on converting PM25 kg/kg to ug/m3'
@@ -149,7 +152,7 @@ for i in range(n_t): #n_t
 
 
 # Use PS top get spatial dimensions of data 
-ncFile = cnm.makeAQNCFile("PS", scenario, 'daily')
+ncFile = cnm.makeAQNCFile(dataDirBase, "SO4_SRF", scenario, tStep="daily")
 nc     = Dataset(ncFile, 'r')
 lat    = nc.variables['lat'][:]
 lon    = nc.variables['lon'][:]
@@ -200,18 +203,18 @@ ncFile.close()
 # TODO: this can be used in a lot of different places already.
 print 'Working on writing the fire emitted PM NETCDF output.'
 
-saveName = cnm.makeAQNCFile('FirePM25', scenario, 'daily')
+saveName = cnm.makeAQNCFile('PM25', scenario, 'daily')
 ncFile = Dataset(saveName, 'w', format='NETCDF4')
-ncFile.description = 'Fire emitted PM2.5 in surface layer'
+ncFile.description = 'PM2.5 in surface layer'
 ncFile.location = 'Global'
 ncFile.createDimension('time', n_t )
 ncFile.createDimension('lat', len(lat) )
 ncFile.createDimension('lon', len(lon) )
 
 # Create variables on the dimension they live on 
-PMVar = ncFile.createVariable('FirePM25', 'f4', ('time','lat','lon'))
+PMVar = ncFile.createVariable('PM25', 'f4', ('time','lat','lon'))
 PMVar.units = 'kg/kg'
-PMVar.description = 'kg of fire emitted PM per kg of air in surface layer. P=Rho*R*T to get dry air concentration estimate.'
+PMVar.description = 'kg PM25 per kg of air in surface layer. P=Rho*R*T to get dry air concentration estimate.'
 
 time_var = ncFile.createVariable('time', 'i4', ('time',))
 time_var.units = 'days from origin'
@@ -223,7 +226,7 @@ longitude = ncFile.createVariable('lon', 'f4', ('lon',))
 longitude.units = 'degrees east'
 
 # Write the actual data to these dimensions
-PMVar[:]         = FirePM25
+PMVar[:]         = PM25
 latitude[:]      = lat
 longitude[:]     = lon
 time_var[:]      = time
