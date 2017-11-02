6cfd9da71ae395f2c5e316035b64ea28cde65d62
Steven <stevenjoelbrey@gmail.com>
2017-11-02 13:37:34 -0600

    re working the integration of daily precipitation.

--- Python/average6HourlyData.py
+++ Python/average6HourlyData.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python2
 
 ###############################################################################
-# ------------------------- Description --------------------------------------- 
+# ------------------------- Description ---------------------------------------
 ###############################################################################
-# This script will be used to generate daily met fields from hourly nc files. 
+# This script will be used to generate daily met fields from hourly nc files.
 # The 6-houly data to average live in /barnes-scratch/sbrey/era_interim_nc_6_hourly
 
-# Follows ---------------------------------------- 
+# Follows ----------------------------------------
 # 	- get_ERA_Interim_data.py
 # Precedes
 #	- merge_yearly_nc.py
@@ -15,6 +15,7 @@
 # ---------------------- Set analysis variables--------------------------------
 ###############################################################################
 import sys
+import cesm_nc_manager as cnm
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument List:', str(sys.argv)
 
@@ -24,16 +25,19 @@ if len(sys.argv) != 1:
 	startYear =  int(sys.argv[2])
 	endYear   =  int(sys.argv[3])
 
-else: 
-	# Development environment. Set variables by manually here. 
+else:
+	# Development environment. Set variables by manually here.
 	hourlyVAR =  'tp'
 	startYear = 2003
 	endYear   = 2003
 
 
-dataDir   = "/barnes-scratch/sbrey/era_interim_nc_6_hourly"
-outputDir = "/barnes-scratch/sbrey/era_interim_nc_daily"
-	
+
+drive = cnm.getDrive()
+
+dataDir   = drive + "era_interim_nc_6_hourly"
+outputDir = drive + "era_interim_nc_daily"
+
 
 print '----------------------------------------------------------------------'
 print 'Working on variable: ' + hourlyVAR + ' for years: ' + str(startYear) +\
@@ -45,7 +49,7 @@ print '----------------------------------------------------------------------'
 import os
 import numpy as np
 from mpl_toolkits.basemap import Basemap, cm
-from netCDF4 import Dataset 
+from netCDF4 import Dataset
 import matplotlib.pyplot as plt
 import numpy.ma as ma
 from datetime import date
@@ -58,27 +62,27 @@ import os.path
 # Start the timer on this code
 timeStart = timer.time()
 
-# Loop over the selected years 
+# Loop over the selected years
 if startYear == 'all':
 	years = ['all']
 else:
 	years  = np.arange(startYear, endYear+1)
 
-ii     = 0 # for counting total iterations	
+ii     = 0 # for counting total iterations
 for year in years:
 
 	year = str(year)
 	print '---------------------------------------------------------------'
 	print 'Working on : ' + year
-	print '---------------------------------------------------------------'	
-	
+	print '---------------------------------------------------------------'
+
 
 	# Load 6-hourly data
-	HourlyFile  = os.path.join(dataDir, hourlyVAR + "_" + year + ".nc")	
+	HourlyFile  = os.path.join(dataDir, hourlyVAR + "_" + year + ".nc")
 	print HourlyFile
 	nc          = Dataset(HourlyFile, 'r')
-	VAR         = nc.variables[hourlyVAR]   
-	time        = nc.variables['time'] 
+	VAR         = nc.variables[hourlyVAR]
+	time        = nc.variables['time']
 	time_hour   = np.array(time[:], dtype=int)
 	lon         = nc.variables['longitude']
 	lat         = nc.variables['latitude']
@@ -97,28 +101,43 @@ for year in years:
 	if time.units == 'hours since 1900-01-01 00:00:0.0':
 		# For time datetime array
 		t0 = datetime.datetime(year=1900, month=1, day=1,\
-                                       hour=0, minute=0, second=0) 
-		# For simply getting dates 
+                                       hour=0, minute=0, second=0)
+		# For simply getting dates
 		date0 = date(year=1900, month=1, day=1)
 
 	else:
-		raise ValueError('Unknown time origin! Code will not work.')		
+		raise ValueError('Unknown time origin! Code will not work.')
 
 	# Create arrays to store datetime objects
 	t = []
 	dates = []
+	year = []
 	for i in range(len(time_hour)):
 		dt = timedelta(hours=time_hour[i])
 		date_new = date0 + dt
-		t_new  = t0 + dt 
+		t_new  = t0 + dt
 		t.append(t_new)
 		dates.append(date_new)
 	t = np.array(t)
 	dates = np.array(dates)
 
+	# Mask out any forecast/analysis hours that occur on the next calendar date.
+	# This needs to be done because of the annoying way ECMWF gives you data for
+	# a selected calendar year.
+	# For precip we download 00:00:00 and 12:00:00 with 12 hour time step to get
+	# total precipitation for a calendar date UTC. If you download say 2003 data
+	# Jan 1 through Dec 31, the firt 12 hour forecast accumulation time you get
+	# is 2003-01-01 12:00:00, not 2003-01-01 00:00:00. The last is
+	# 2004-01-01 00:00:00, not 2003-12-31 12:00:00.
+	# Example 1 here https://software.ecmwf.int/wiki/pages/viewpage.action?pageId=56658233
+	# makes it seem as though you need to sum the 00:00:00 and 12:00:00 values
+	# to get daily total precipitation.
+
+
+
 	# Create structure to save daily data and do the averaging
 	unique_dates = np.unique(dates)
-	nDays = len(unique_dates)
+	nDays = len(unique_dates) # might have to do a - 1 here now. Or search for feb 29th and set length based on that.
 	nLon  = len(lon)
 	nLat  = len(lat)
 
@@ -140,7 +159,7 @@ for year in years:
 	for i in range(nDays):
 		ii = ii + 1.
 		print str(ii/(float(nDays)*len(years))*100.) + " % complete"
-		
+
 		# find unique day to work on
 		indexMask = np.where(dates == unique_dates[i])[0]
 
@@ -152,27 +171,39 @@ for year in years:
 
 		elif (len(dims) == 3) & (hourlyVAR == 'tp'):
 
-			# Precip units of m per 12 hour window. Requires a sum not an average.
+			# Precip units of m per 12 hour window. Requires a sum NOT an average.
 			VAR_array_subset = VAR_array[indexMask, :, :]
 			day_time_total = np.sum(VAR_array_subset, axis=0)
-			dailyVAR[i, :, : ] = day_time_total		
-			print 'treating precip differently'	
-		
+			dailyVAR[i, :, : ] = day_time_total
+			#print 'treating precip differently'
+
 		else:
-			# Non-precip variables of this size need an average. 
+
+			# Non-precip variables of this size need an average.
 			VAR_array_subset = VAR_array[indexMask, :, :]
 			day_time_mean = np.mean(VAR_array_subset, 0)
-			dailyVAR[i, :, : ] = day_time_mean	
+			dailyVAR[i, :, : ] = day_time_mean
 
 	meansCompleteTime = timer.time()
-	dt = (meansCompleteTime - timeStart) / 60. 
+	dt = (meansCompleteTime - timeStart) / 60.
 	print '----------------------------------------------------------------------'
 	print 'It took ' + str(dt) + ' minutes to create daily averages.'
 	print '----------------------------------------------------------------------'
 
+
+	# Check to see if the total amount of precip was conserved.
+	if hourlyVAR == 'tp':
+		sixHourSum = np.sum(VAR, axis=0)
+		dailySum   = np.sum(dailyVAR, axis=0)
+		dtp = sixHourSum - dailySum
+		# ideally dtp is all zero. With float rounding issues it could be slightly
+		# different. This matrix needs to be examined.
+		print 'Maximum annual difference in rainfall is: ' + str(np.max(dtp))
+
+
 	###############################################################################
 	# Write the new netcdf data with the exact same formatting as the
-	# data read here. 
+	# data read here.
 	# Create the Save name and make sure that this file does not exist
 	###############################################################################
 
@@ -195,8 +226,8 @@ for year in years:
 	ncFile.createDimension('time',  nDays )
 	ncFile.createDimension('latitude', nLat )
 	ncFile.createDimension('longitude', nLon )
-	
-	# Create variables on the dimension they live on 
+
+	# Create variables on the dimension they live on
 	if len(dims) == 4:
 
 		ncFile.createDimension('level',  nLevel )
@@ -215,12 +246,13 @@ for year in years:
                             fill_value=VAR._FillValue)
 
 	# Assign the same units as the loaded file to the main variable
+	# NOTE: Does this change values? Since
 	dailyVAR_.units = VAR.units
 	dailyVAR_.scale_factor = VAR.scale_factor
 	dailyVAR_.add_offset = VAR.add_offset
 	dailyVAR_.missing_value = VAR.missing_value
 
-	# Create time variable	
+	# Create time variable
 	time_ = ncFile.createVariable('time', 'i4', ('time',))
 	time_.units = time.units
 	time_.calendar = time.calendar
@@ -228,7 +260,7 @@ for year in years:
 	# create lat variable
 	latitude_ = ncFile.createVariable('latitude', 'f4', ('latitude',))
 	latitude_.units = lat.units
-	
+
 	# create longitude variable
 	longitude_ = ncFile.createVariable('longitude', 'f4', ('longitude',))
 	longitude_.units = lon.units
@@ -244,14 +276,14 @@ for year in years:
 	# NOTE: are 4 sets of 6 hourly data for any given date. However, tp
 	# NOTE: (total precip) only has two chunks of 6 hourly data per day.
 	# NOTE: so this needs to be handled seperately.
-	tstep = len(time) / nDays	
-	time_[:] = time[0::tstep] 
-	# The difference in each time_[:] element in hours must be 24 or 
+	tstep = len(time) / nDays
+	time_[:] = time[0::tstep]
+	# The difference in each time_[:] element in hours must be 24 or
 	# this is not working.
 	if np.unique(np.diff(time_[:])) != 24:
 		ValueError('Difference in hours between days not all 24! Broken.')
 
-	# The data is written, close the ncFile and move on to the next year! 
+	# The data is written, close the ncFile and move on to the next year!
 	ncFile.close()
 
 
