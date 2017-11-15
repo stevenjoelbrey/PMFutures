73c1dce06a128b022955d5fc14d1d6ea4dd449f0
Steven <stevenjoelbrey@gmail.com>
2017-11-15 13:23:00 -0700

    script for merging ecoregion assignment calculations that are done annually to speed up the calculation.

---
+++ R/merge_ecoregion_year_data.R
@@ -0,0 +1,64 @@
+# merge_ecoregion_year_data.R
+
+# assign_ecoregion_to_FPAFOD.R assigns each fire event the ecoregion it falls
+# into. To make this way faster, this is done one year at a time. This script
+# will be used to sanity check the assignments and also to merge these yearly 
+# files together. Really this whole script and seperation of the former is
+# only needed because I do not know how to parallel program in R. 
+
+library(maps)
+library(sp)
+library(rgdal)
+library(maptools)
+library(rgeos)
+
+dataDir   <- "Data/FPA_FOD/"
+startYear <- 2003
+endYear   <- 2013
+years <- startYear:endYear
+
+# Loop through and load desired files. 
+for (year in years){
+  
+  f <- paste0(dataDir, "FPA_FOD_borderfix_",year,".RData")
+  
+  if(year == years[1]){
+    FPA_FOD_merged <- get(load(f))
+  } else{
+    FPA_FOD        <- get(load(f))
+    FPA_FOD_merged <- rbind(FPA_FOD_merged, FPA_FOD)
+  }
+  
+}
+
+print("Unique years in the merged dataset include..")
+print(unique(FPA_FOD_merged$FIRE_YEAR))
+
+
+FPA_FOD_merged$NA_L2CODE <- as.numeric(as.character(FPA_FOD_merged$NA_L2CODE))
+
+# Need to handle the points that feel inbetween polygons. Assignments must be
+# made for these ~ 33,000 fires. 
+
+# TODO: Remove peurto rico fires, these are outside our scope and do not have
+# TODO: ecoregions. AND HAWAII
+
+
+print(sum(is.na(FPA_FOD_merged$NA_L2CODE)))
+
+
+fireLocations <- SpatialPoints(coordinates)
+
+map("world", ylim=c(10,80), xlim=c(-170, -50))
+# Any NAs? Where are they
+plot(fireLocations[is.na(FPA_FOD_merged$NA_L2CODE),], add=T, col="red", pch=".")
+FPA_FOD_merged$NA_L2CODE[is.na(FPA_FOD_merged$NA_L2CODE)] <- 0
+plot(fireLocations[FPA_FOD_merged$NA_L2CODE==6.2,], add=T, col="forestgreen",pch=".")
+
+# Alaska and Northwest coast
+plot(fireLocations[FPA_FOD_merged$NA_L2CODE==7.1,], add=T, col="lightblue",pch=".")
+# Great Basin and Plains of Western Wyoming and four corners. 
+plot(fireLocations[FPA_FOD_merged$NA_L2CODE==10.1,], add=T, col="tan",pch=".")
+# South tip of Florida
+plot(fireLocations[FPA_FOD_merged$NA_L2CODE==15.4,], add=T, col="purple",pch=".")
+
