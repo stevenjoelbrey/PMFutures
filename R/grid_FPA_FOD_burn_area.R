dd19916954ddf86ade7aaa844915a7bf38cde487
Steven <stevenjoelbrey@gmail.com>
2017-11-27 10:32:05 -0700

    typo, which.max instead of which.min used in grid finder. Error was obvious in output.

--- R/grid_FPA_FOD_burn_area.R
+++ R/grid_FPA_FOD_burn_area.R
@@ -3,18 +3,22 @@ args = commandArgs(trailingOnly=TRUE)
 if(length(args)==0){
   year1 <- 2003
   year2 <- 2013
+  sanityCheck <- FALSE
 } else{
   year1 <- args[1]
   year2 <- args[2]
+  sanityCheck <- as.logical(args[3])
 }
 
 # grid_FPA_FOD_burn_area.R
+# TODO: Sum all area burned in dataframe to be gridded and grid. They need to be
+# TODO: the same number for us to know the gridding happenned correctly!
 
-###############################################################################
 # ------------------------- Description ---------------------------------------
-###############################################################################
 # This script will be used to grid monthly burn area of FPA_FOD data. The grid 
 # will match GFED4s.
+
+# TODO: Check me again! This was written fast and not reviewed! 
 
 print(paste("Working on year:", year1, "to year", year2))
 
@@ -28,7 +32,6 @@ library(raster)
 library(ncdf4)
 library(geosphere)
 
-
 # Get the quarter degree grid that all data lives on (GFED4s grid). All attribute
 # layers will live on the same lat lon grid as this. 
 fgrid <- "Data/GFED4s/GFED4.1s_burned_area_2004.nc"
@@ -38,7 +41,7 @@ longitude <- ncvar_get(nc, "longitude")
 latitude  <- ncvar_get(nc, "latitude")
 nc_close(nc)
 
-# Create the time dimension
+# Create the monthly time dimension, represented by a single date of that month
 t1 <- as.POSIXct(paste0(year1,"-01-01"), tz="UTC")
 t2 <- as.POSIXct(paste0(year2,"-12-31"), tz="UTC")
 time <- seq(t1,t2, by="month")
@@ -51,7 +54,8 @@ gridYear  <- timeLT$year + 1900
 # Define the length of the monthly time dimension, [years X months]
 nTime <- length(year1:year2) * length(1:12)
 
-# Load the FPA_FOD_data
+# Load the "FPA_FOD" dataframe. These data have all desired appended information
+# NOTE: This file has minDate = "2003-01-01 UTC" and maxDate = "2013-12-31 UTC"
 load(paste0("Data/FPA_FOD/FPA_FOD.RData"))
 
 # Get fire info 
@@ -59,12 +63,13 @@ fireLon  <- FPA_FOD$LONGITUDE
 fireLat  <- FPA_FOD$LATITUDE
 fireTime <- FPA_FOD$DISCOVERY_DATE
 fireStartMonth  <- as.POSIXlt(FPA_FOD$DISCOVERY_DATE)$mon + 1
-fireStartYear <- FPA_FOD$FIRE_YEAR
+fireStartYear   <- FPA_FOD$FIRE_YEAR # came prepackaged with original FPA-FOD
 containmentDate <-FPA_FOD$CONT_DATE
 fireEndMonth    <- as.POSIXlt(FPA_FOD$CONT_DATE)$mon + 1
 cause <- FPA_FOD$STAT_CAUSE_DESCR
 nFires   <- dim(FPA_FOD)[1]
 
+# ------------------------- fireStats ------------------------------------------
 # For fires that have containment dates, what % end in the same month they 
 # start? This is an important piece of information for describing monthly burn
 # area product you are about to make. 
@@ -81,14 +86,17 @@ print(paste("Percent of fires with containment dates that end same as start mont
 # Of the fires that occur in one month, what % of burn area do these account for? 
 totalBurnArea <- sum(FPA_FOD$FIRE_SIZE[!noContainmentDate])
 sameMonthBurnArea <- sum(FPA_FOD$FIRE_SIZE[!noContainmentDate][deltaMonthContain==0])
+fSame <- sameMonthBurnArea / totalBurnArea * 100
+print(paste("Percent of burn area from fires that have same month containment dates:", fSame))
 
-
+# ------------------------- fireCause sorting ----------------------------------
 # Now handle the cause, which I will track for monthly gridded burn area
 # product
-causeSimple <- rep("", nFires)
+
+causeSimple   <- rep("", nFires)
 lightningMask <- "Lightning" == cause
-unkownMask <- "Missing/Undefined" == cause
-humanMask <- !lightningMask & !unkownMask
+unkownMask    <- "Missing/Undefined" == cause
+humanMask     <- !lightningMask & !unkownMask
 
 # These are the only three categories we want to track 
 causeSimple[lightningMask] <- "Lightning"
@@ -96,35 +104,46 @@ causeSimple[unkownMask] <- "Unknown"
 causeSimple[humanMask] <- "Human"
 
 # Create array to save monthly burn area at each location. 
-human_burn_area <- array(data=0, dim=c(nTime, length(longitude), length(latitude)))
-lightning_burn_aera <- human_burn_area
-unknown_burn_area <- human_burn_area
+nLon <- length(longitude)
+nLat <- length(latitude)
+human_burn_area     <- array(data=0, dim=c(nTime, nLon, nLat))
+lightning_burn_aera <- array(data=0, dim=c(nTime, nLon, nLat))
+unknown_burn_area   <- array(data=0, dim=c(nTime, nLon, nLat))
 
-################################################################################
+# ------------------------- Loop -----------------------------------------------
 # Loop through each fire, placing it in the correct grid (space & time)
-################################################################################
+
 metersSquaredPerAcre <- 4046.86
+df_burn_area_sum <- sum(FPA_FOD$FIRE_SIZE * 4046.86)
+
 for (i in 1:nFires){# nFires
   
-  # Get the burn area
+  # Get the burn area for this fire
   BA <- FPA_FOD$FIRE_SIZE[i] * metersSquaredPerAcre 
   
-  # Find this fires place in time 
+  # Find this fires place in time, i.e., correct year and month 
   tMask <- (fireStartMonth[i] == gridMonth) & (fireStartYear[i] == gridYear)
   
   # Difference in array distances
   dx <- abs(longitude - fireLon[i])
   dy <- abs(latitude - fireLat[i])
   xi <- which.min(dx)
-  yi <- which.max(dy)
+  yi <- which.min(dy)
+  
+  # Map where I am gridding the point when I am checking in development mode
+  if(sanityCheck){
+    plot(longitude[xi], latitude[yi], col="black", pch=19)
+    points(fireLon[i], fireLat[i], pch=3, col="red")
+    map("state", add=T)
+  }
   
   # Place the burn area in the correct grid box based on ignition cause 
-  cause <- causeSimple[i]
-  if(cause == "Lightning"){
+  thisCause <- causeSimple[i]
+  if(thisCause == "Lightning"){
     lightning_burn_aera[tMask, xi, yi] <- lightning_burn_aera[tMask, xi, yi] + BA 
-  } else if(cause == "Unknown"){
+  } else if(thisCause == "Unknown"){
     unknown_burn_area[tMask, xi, yi] <- unknown_burn_area[tMask, xi, yi] + BA 
-  } else if(cause == "Human"){
+  } else if(thisCause == "Human"){
     human_burn_area[tMask, xi, yi] <- human_burn_area[tMask, xi, yi] + BA
   } else{
     stop("Ignition category of this fire is not known. Find cause and rewrite.")
@@ -139,6 +158,12 @@ for (i in 1:nFires){# nFires
 
 # Add these three types together for fast access to a total burn_area variable.
 burn_area <- lightning_burn_aera + unknown_burn_area + human_burn_area
+
+# Now see if the total burn area was conserved or not
+gridded_burn_area_sum <- sum(burn_area)
+dArea <- gridded_burn_area_sum - df_burn_area_sum
+print(paste("The difference in meters squared before and after gridding is:", dArea))
+
 
 # The nc file needs a nice descriptive time dimension
 t0 <- as.POSIXct("1900-01-01 00:00:00", tz="UTC")
@@ -163,7 +188,7 @@ ncfname <- paste(ncpath, ncname, ".nc", sep="")
 # define dimensions
 londim <- ncdim_def("longitude","degrees_east", as.double(longitude)) 
 latdim <- ncdim_def("latitude","degrees_north", as.double(latitude)) 
-timedim <- ncdim_def("time", "hours since 1900-01-01 00:00:0.0", dt_hours)
+timedim <- ncdim_def("time", "YYYYMM of burn area", YYYYMM, TRUE) # unlimited == TRUE
 
 # define variables
 fillvalue <- 1e32
@@ -173,17 +198,18 @@ grid_area.def <- ncvar_def("grid_area","m**2",list(londim,latdim),fillvalue,dlna
 dlname <- "YYYYMM"
 YYYYMM.def <- ncvar_def("YYYYMM","month",list(timedim),fillvalue,dlname,prec="float")
 
+dimList <- list(timedim,londim,latdim)
 dlname <- "burn_area"
-burn_area.def <- ncvar_def("burn_aera","m**2",list(timedim,londim,latdim),fillvalue,dlname,prec="float")
+burn_area.def <- ncvar_def("burn_aera","m**2",dimList,fillvalue,dlname,prec="float")
 
 dlname <- "lightning_burn_aera"
-lightning_burn_aera.def <- ncvar_def("lightning_burn_aera","m**2",list(timedim,londim,latdim),fillvalue,dlname,prec="float")
+lightning_burn_aera.def <- ncvar_def("lightning_burn_aera","m**2",dimList,fillvalue,dlname,prec="float")
 
 dlname <- "unknown_burn_area"
-unknown_burn_area.def <- ncvar_def("unknown_burn_area","m**2",list(timedim,londim,latdim),fillvalue,dlname, prec="float")
+unknown_burn_area.def <- ncvar_def("unknown_burn_area","m**2",dimList,fillvalue,dlname, prec="float")
 
 dlname <- "human_burn_area"
-human_burn_area.def <- ncvar_def("human_burn_area","m**2",list(timedim,londim,latdim),fillvalue,dlname,prec="float")
+human_burn_area.def <- ncvar_def("human_burn_area","m**2",dimList,fillvalue,dlname,prec="float")
 
 
 # create netCDF file and put arrays. All sperarate working. Together not. 
