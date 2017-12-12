6cebbe3a56884766affb2916c40a28e19d288f5b
Steven <stevenjoelbrey@gmail.com>
2017-12-12 14:36:06 -0700

    engineering axis added and functions changed to make plotting fire envirinmental distributions easier to subset.

--- R/plot_ignition_weather.R
+++ R/plot_ignition_weather.R
@@ -5,6 +5,7 @@
 
 # Load the required libraries
 library(maps)
+library(sfsmisc)
 
 load("Data/FPA_FOD/FPA_FOD_ecmwf_2003_2013.RData")
 FPA_FOD$t2m <- FPA_FOD$t2m - 273.15 # C is much nicer
@@ -21,7 +22,7 @@ minLon <- -125
 maxLon <- -100
 
 # acres
-minFireSize <- 0 
+minFireSize <- 1000 
 
 # Season
 includeMonths <- c(5:10)
@@ -29,7 +30,7 @@ includeMonths <- c(5:10)
 # ecoregion 
 ecoregion <- 6.2
 # Leave this line uncommented if you want to retain all ecoregions
-ecoregion <- unique(FPA_FOD$NA_L2CODE)
+#ecoregion <- unique(FPA_FOD$NA_L2CODE)
 
 
 lat <- FPA_FOD$LATITUDE
@@ -60,23 +61,6 @@ FPA_FOD_subset <- FPA_FOD[m,]
 fireCause <- FPA_FOD_subset$STAT_CAUSE_DESCR
 humanMask <- fireCause != "Lightning"
 
-# Seperate ignition location variables by the humanMask vs. !humanMask
-t2m_human     <- density(FPA_FOD_subset$t2m[humanMask])
-t2m_lightning <- density(FPA_FOD_subset$t2m[!humanMask])
-
-rh2m_human     <- density(FPA_FOD_subset$rh2m[humanMask]) 
-rh2m_lightning <- density(FPA_FOD_subset$rh2m[!humanMask]) 
-
-dsr_human     <- density(FPA_FOD_subset$days_since_rain[humanMask]) 
-dsr_lightning <- density(FPA_FOD_subset$days_since_rain[!humanMask]) 
-
-tp_human     <- density(FPA_FOD_subset$tp[humanMask]) 
-tp_lightning <- density(FPA_FOD_subset$tp[!humanMask]) 
-
-elev_human <- density(FPA_FOD_subset$elevation[humanMask]) 
-elev_lightning <- density(FPA_FOD_subset$elevation[!humanMask]) 
-
-
 # t2m
 # rh2m
 # days_since_rain
@@ -84,8 +68,11 @@ elev_lightning <- density(FPA_FOD_subset$elevation[!humanMask])
 # mean temperature last 10 days
 # total rain last 10 days. 
 
+
+
+
 # Function for plotting two distributions
-plot_ignition_weatther_distribution <- function(VAR="t2m", varLabel, humanMask){
+plot_ignition_weather_distribution <- function(VAR="t2m", varLabel, humanMask){
   
   # Create the density estimates for the two types. 
   dHuman     <- density(FPA_FOD_subset[[VAR]][humanMask])
@@ -104,13 +91,15 @@ plot_ignition_weatther_distribution <- function(VAR="t2m", varLabel, humanMask){
        lwd=4,
        bty="n", 
        cex.axis=2,
-       cex.lab=2)
+       cex.lab=2,
+       yaxt="n")
+  eaxis(2, cex.axis=2)
   
   mtext("Probability Density", 2,  line=5, cex=2)
   
   lines(dLightning$x, dLightning$y, col="darkgray", lwd=4)
   
-  title(paste("Mean Daily", varLabel, "at Ignition Locations"), cex.main=2)
+  title(paste("Mean", varLabel, "at Ignition Locations"), cex.main=1.4)
   
   legend_human <- paste("Human, n=", sum(humanMask))
   legend_lightning <- paste0("Lightning, n=", sum(!humanMask))
@@ -126,39 +115,48 @@ plot_ignition_weatther_distribution <- function(VAR="t2m", varLabel, humanMask){
 
 }
 
-plot_ignition_weatther_distribution(t2m_human, t2m_lightning, "Temperature [K]", humanMask)
-plot_ignition_weatther_distribution(rh2m_human, rh2m_lightning, "RH %", humanMask)
-plot_ignition_weatther_distribution(dsr_human, dsr_lightning, "days since rain > 0 inch", humanMask)
-plot_ignition_weatther_distribution(tp_human, tp_lightning, "Total Precip [m]", humanMask)
-
-plot_ignition_weatther_distribution(elev_human, elev_lightning, "[m]", humanMask)
+quartz(width=15, height=5)
 
 
+png(filename=paste0("Figures/summary/ignitionTemperature_6_2_Fires>1000.png"),
+    width=3000, height=1000, res=200)
+par(mfrow=c(1,3), mar=c(4,5,4,4))
+plot_ignition_weather_distribution("t2m", "Temperature [K]", humanMask)
+
+# Show the locations of the fires
 # TODO: Dot size is size of fire!
 map("state", xlim=c(-125,-102), ylim=c(33,50))
 points(FPA_FOD_subset$LONGITUDE[humanMask], FPA_FOD_subset$LATITUDE[humanMask], 
-       col="red", pch=".")
-
+       col="orange", pch=".")
 points(FPA_FOD_subset$LONGITUDE[!humanMask], FPA_FOD_subset$LATITUDE[!humanMask], 
-       col="black", pch=".")
-title(paste("Fires in ecoregion", ecoregion))
+       col="darkgray", pch=".")
+title(paste("Fires locations"), cex.main=2)
+
 
 # TODO: histogram of the JDay that these fires are occuring on. 
 minDay <- min(FPA_FOD_subset$DISCOVERY_DOY)
 maxDay <- max(FPA_FOD_subset$DISCOVERY_DOY)
 
 hist(FPA_FOD_subset$DISCOVERY_DOY[humanMask], 
-     col=adjustcolor("red",0.5),
+     col=adjustcolor("orange",0.5),
      breaks=c(minDay:maxDay), las=1, xlab="Day of Year",
-     ylim=c(0,1200),
+     ylim=c(0,30),
      border="transparent",
-     main="Day of year ignition counts")
+     main="Day of year ignition counts",
+     cex.main=2)
 hist(FPA_FOD_subset$DISCOVERY_DOY[!humanMask], 
-     col=adjustcolor("black", 0.5), breaks=c(minDay:maxDay), add=T,
+     col=adjustcolor("darkgray", 0.5), breaks=c(minDay:maxDay), add=T,
      border="transparent")
+dev.off()
+
+plot_ignition_weather_distribution("tp_lastMonth", "Total Precip", humanMask)
 
 
 
+quartz()
+plot(FPA_FOD_subset$windSpeed_MonthAfter, FPA_FOD_subset$FIRE_SIZE)
+
+#windSpeed_MonthAfter
 
 
 
