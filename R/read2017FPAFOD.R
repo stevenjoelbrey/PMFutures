262d6e6e630601de225c0ffc1b665932a4fd656d
Steven <stevenjoelbrey@gmail.com>
2017-12-21 11:18:39 -0700

    fixes to handling of dates for new FPA-FOD years (2014-2015)

--- R/read2017FPAFOD.R
+++ R/read2017FPAFOD.R
@@ -14,9 +14,9 @@
 library(sp) 
 library(rgdal)
 
-localDir <- "Data/GIS/FPAFOD20170508/"
-layerName <- "FPAFOD20170508"
-data_projected <- readOGR(dsn=localDir, layer=layerName) 
+externalDir <- "/Volumes/Brey_external/FPAFOD20170508"
+layerName   <- "FPAFOD20170508"
+data_projected <- readOGR(dsn=externalDir, layer=layerName) 
 
 df <- data_projected@data
 nCol <- length(names(df))
@@ -27,13 +27,51 @@ load("Data/FPA_FOD/FPA_FOD_2003_2013.RData")
 # NA_L2CODE
 names(df) <- names(FPA_FOD)[1:nCol]
 
-write.csv(df, file="Data/FPA_FOD/FPA_FOD_1992_2015.csv", row.names=FALSE)
+write.csv(df, file="TEMP.csv", row.names=FALSE)
 
 # This will automatically handle the pesky factors that come out of the projected
 # data
-df <- read.csv("Data/FPA_FOD/FPA_FOD_1992_2015.csv", 
+df <- read.csv("TEMP.csv", 
                stringsAsFactors = FALSE) 
+
+################################################################################
+# ------- Now that we have a non-factor version HANDLE THE DATES ---------------
+################################################################################
+
+# Get start time, containment time, and out time into POSIXct format
+disc_mmddyy_TIME    <- df$DISCOVERY_DATE # "YYYY/MM/DD 00:00:00.000" <- format 
+spaceLocation       <- str_locate(disc_mmddyy_TIME, " ")
+
+# The time of these incidents is not needed. Just the date. These data are not 
+# that precise. 
+disc_mmddyy    <- str_sub(disc_mmddyy_TIME, 1, spaceLocation[,1])
+
+# Get rid of the space that sometimes shows up at the end 
+disc_mmddyy_noSpace <- str_replace(disc_mmddyy, " ", "")
+discoverd_date <- as.POSIXct(disc_mmddyy_noSpace, format="%Y/%m/%d", tz="UTC")
+
+# Add to dataframe
+df$DISCOVERY_DATE <- discoverd_date
+
+# Repeat this method for the containment date
+con_mmddyy_TIME <- df$CONT_DATE
+spaceLocation <- str_locate(con_mmddyy_TIME, " ")
+con_mmddyy <- str_sub(con_mmddyy_TIME, 1, spaceLocation[,1])
+con_mmddyy_noSpace  <- str_replace(con_mmddyy, " ", "")
+con_date <- as.POSIXct(con_mmddyy_noSpace, format="%Y/%m/%d", tz="UTC")
+
+# Store in the dataframe
+df$CONT_DATE <- con_date
+
+# Make an array of the month of the start date 
+t_LT <- as.POSIXlt(discoverd_date)
+month <- t_LT$mon + 1
+
+# Add it to the dataframe
+df$START_MONTH <- month
+
 
 # Now write the factor free version of the dataframe as .RData file
 FPA_FOD <- df
-save(FPA_FOD, file="Data/FPA_FOD/FPA_FOD_1992_2015.RData")
+save(FPA_FOD, file="Data/FPA_FOD/FPA_FOD_1992_2015_noEcoregion.RData")
+write.csv(FPA_FOD, file="Data/FPA_FOD/FPA_FOD_1992_2015_noEcoregion.csv")
