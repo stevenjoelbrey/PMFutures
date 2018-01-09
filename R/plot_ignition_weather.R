008badfbaa12cbba8c4a52bd3fa184605bb7b7fb
Steven <stevenjoelbrey@gmail.com>
2018-01-09 13:40:10 -0700

    saving each Jday comparison figure and creating an experiment directory for each unique set of parameters.

--- R/plot_ignition_weather.R
+++ R/plot_ignition_weather.R
@@ -4,6 +4,8 @@
 # This script will be used to determine if the weather on ignition days is 
 # different for different ignition types. 
 
+# TODO: eliminate small fires. (e.g. less than 1 acre). What do we learn?
+
 # Load the required libraries
 library(maps)
 library(sfsmisc)
@@ -16,17 +18,29 @@ maxLat <- 50
 minLon <- -125
 maxLon <- -100
 
-# acres
-minFireSize <- 1000 
-
 # Season
 includeMonths <- c(5:10)
 
 # ecoregion 
 ecoregion <- 6.2
 
-# Leave this line uncommented if you want to retain all ecoregions
-#ecoregion <- unique(FPA_FOD$NA_L2CODE)
+# Min fire size. Recal, even though fire managers view every small fire as a 
+# potentially large fire, in terms on when real emissions occur, there is a 
+# good reason to ignore lots of really small fires. 
+minFireSize <- 0 
+
+# THESE ARGUMENTS SET UP A UNIQUE EXPERIMENT. MAKE A DIRECTORUY TO STORE EACH
+# EXISTING EXPERIMENT. 
+experimentVARS <- paste0("ecoregion=", ecoregion,"_",
+                         "months=", min(includeMonths), "-", max(includeMonths), "_",
+                         "sizeMin=", minFireSize
+                         )
+
+experimentDir <- paste0("Figures/ignitionCompare_", experimentVARS, "/")
+if(!dir.exists(experimentDir)){
+  dir.create(experimentDir)
+}
+
 
 dataFile <- paste0("Data/FPA_FOD/FPA_FOD_ecmwf_1992_2015.RData")
 load(dataFile)
@@ -49,17 +63,69 @@ timeMask <- FPA_FOD$START_MONTH %in% includeMonths
 # Ecoregion ecoregion
 ecoregionMask <- FPA_FOD$NA_L2CODE %in% ecoregion
 
-# # Fire size mask 
-# sizeMask <- FPA_FOD$FIRE_SIZE >= minFireSize
+# Fire size mask 
+sizeMask <- FPA_FOD$FIRE_SIZE >= minFireSize
 
 # Subset the data, one mask to rule them all. 
-m <- timeMask & latMask & lonMask & ecoregionMask 
+m <- timeMask & latMask & lonMask & ecoregionMask & sizeMask
 FPA_FOD <- FPA_FOD[m,]
 
 # Get the cuase so we can create comparisons 
 fireCause     <- FPA_FOD$STAT_CAUSE_DESCR
 lightningMask <- fireCause == "Lightning"
 
+# Function for plotting two distributions
+plot_ignition_weather_distribution <- function(VAR="t2m", varLabel, humanMask){
+  
+  # Create the density estimates for the two types. 
+  dHuman     <- density(FPA_FOD[[VAR]][humanMask])
+  dLightning <- density(FPA_FOD[[VAR]][!humanMask])
+  
+  par(las=1)
+  
+  # Get the needed max value for the y-axis
+  maxVal <- max(c(dHuman$y,  dLightning$y))
+  
+  plot(dHuman$x, dHuman$y, 
+       type="l", col='orange',
+       xlab=varLabel,
+       ylab="", 
+       ylim=c(0, maxVal),
+       lwd=4,
+       bty="n", 
+       cex.axis=2,
+       cex.lab=2,
+       yaxt="n")
+  eaxis(2, cex.axis=2)
+  
+  mtext("Probability Density", 2,  line=5, cex=2)
+  
+  lines(dLightning$x, dLightning$y, col="darkgray", lwd=4)
+  
+  title(paste("Mean", varLabel, "at Ignition Locations"), cex.main=1.4)
+  
+  legend_human <- paste("Human, n=", sum(humanMask))
+  legend_lightning <- paste0("Lightning, n=", sum(!humanMask))
+  
+  legend("topleft",
+         legend=c(legend_human, legend_lightning),
+         col=c("orange", "darkgray"),
+         lty=1,
+         cex=1,
+         lwd=4,
+         bty="n")
+  
+  
+}
+
+# Show the distribution of environmental variables for two ignition types
+# for all elevations and days to motivated subset comparison
+
+png(filename=paste0(experimentDir, "t2m_distribution_all.png"),
+    width=2500, height=2200, res=250)
+par(mar=c(5,5,5,5))
+plot_ignition_weather_distribution("t2m", "Temperature [C]", !lightningMask)
+dev.off()
 ################################################################################
 # Sample mean differences vs. Julain day
 ################################################################################
@@ -70,21 +136,23 @@ minSampleSize <- 50
 elevationBins <- seq(-300, 3900, by=300)
 nBins         <- length(elevationBins)
 
-# TODO: preset the elevation bins and store differences for these bins to see
-# TOOD: if differences in ignition temp occur at different elevations
-
+# Get the fire occurence day of year for ignitions information 
 JDay       <- FPA_FOD$DISCOVERY_DOY
 JDaysArray <- sort(unique(JDay))
 nDays      <- length(JDaysArray)
 
 # Find the edges of the julain day experiment using a histogram of JDay counts
 # TODO: Do this by ignition type 
-hist(JDay, breaks = JDaysArray, main="Daily Ignition Counts: 1992-2015", las=1)
-hist(JDay[!lightningMask], breaks = JDaysArray, col="orange", add=T)
+png(filename="Figures/summary/JDayIgnitionCounts.png",
+    res=250, height=2000, width=3700)
+hist(JDay, breaks = JDaysArray, main="Julian Day Ignition Counts: 1992-2015", las=1)
+hist(JDay[!lightningMask], breaks = JDaysArray, col="orange", add=T, xlim=c(0, 370))
 span <- as.numeric(quantile(JDay, c(0.10, 0.90) ) ) 
 abline(v=span, col="red", lwd=3)
 legend("topleft", legend=c("Total Ignitions", "Human Ignitions"),
        fill=c("white", "orange"), bty="n", cex=2)
+
+dev.off()
 
 # Define the range explicitly 
 minJDay <- span[1]
@@ -115,7 +183,8 @@ df_t_test <- df_dummy
 
 # Loop through each day and perform experiment
 # TODO: Should I loop over every JDay or by the tolerance? Probably Tol.
-for (i in 1:length(loopJDays)){ 
+# TODO: otherwise we are reusing a lot of data. 
+for (i in 1:nLoop){ 
   
   # Subset fires to a given julain day (or maybe range of +-10 or something?)
   JDayMask <- abs(JDay - loopJDays[i]) < JDayTol
@@ -125,15 +194,22 @@ for (i in 1:length(loopJDays)){
   df <- FPA_FOD[JDayMask,]
   lMask <- df$STAT_CAUSE_DESCR == "Lightning"
   
+  # Default cover is orange until seen that it is lightning 
   COL <- rep("orange", sum(JDayMask))
   COL[lMask] <- "gray"
   
   # TODO: boxplot of these data by elevation bin! 
   # TODO: Also show the 
-  plot(df$elevation, df$t2m, col=COL, pch=19, cex=0.5, 
+  jDayFilename <- paste0("Figures/JDayIgnitionTvsElevation/JDay_",
+                         loopJDays[i],".png")
+  png(filename=jDayFilename, res=250, height=2000, width=4000)
+  
+  plot(df$elevation, df$t2m, 
+       xlab="Elevation [m]", ylab="Temperature [C]",
+       col=COL, pch=19, cex=0.5, 
        ylim=c(0, 30))
   abline(v=elevationBins, lty=2)
-  title(paste("Julain Day Center:", loopJDays[i]))
+  title(paste("Julain Day Center:", loopJDays[i], "+-", JDayTol))
   
   # All elevations difference of means 
   meanLT <- mean(df$t2m[lMask])
@@ -143,6 +219,8 @@ for (i in 1:length(loopJDays)){
   abline(h=meanHT, col="orange", lwd=3)
   
   allDT <- meanLT - meanHT
+  
+  dev.off()
   
   # Now account for elevation 
   # IDEA: Adjust temperature based on elevation using standard lapse rate?
@@ -223,8 +301,9 @@ for (i in 1:length(loopJDays)){
 ################################################################################
 # Histogram of differences chunked by Julain day and elevation
 ################################################################################
-pdf(file="diffence_by_elevation_bin.pdf", width=3, height=20)
-par(mfrow=c(10,1))
+pdf(file="Figures/summary/diffence_of_means_by_elevation_bin.pdf", 
+    width=12, height=20)
+par(mfrow=c(5,2))
 # TODO: Show the total number of fires that went into the calculation for
 # TODO: each elevation bin. Segregated by ignition type. 
 # columns 3:12
@@ -279,49 +358,6 @@ dev.off()
 # Regular Distributions 
 ################################################################################
 
-# Function for plotting two distributions
-plot_ignition_weather_distribution <- function(VAR="t2m", varLabel, humanMask){
-  
-  # Create the density estimates for the two types. 
-  dHuman     <- density(FPA_FOD_subset[[VAR]][humanMask])
-  dLightning <- density(FPA_FOD_subset[[VAR]][!humanMask])
-  
-  par(las=1)
-  
-  # Get the needed max value for the y-axis
-  maxVal <- max(c(dHuman$y,  dLightning$y))
-  
-  plot(dHuman$x, dHuman$y, 
-       type="l", col='orange',
-       xlab=varLabel,
-       ylab="", 
-       ylim=c(0, maxVal),
-       lwd=4,
-       bty="n", 
-       cex.axis=2,
-       cex.lab=2,
-       yaxt="n")
-  eaxis(2, cex.axis=2)
-  
-  mtext("Probability Density", 2,  line=5, cex=2)
-  
-  lines(dLightning$x, dLightning$y, col="darkgray", lwd=4)
-  
-  title(paste("Mean", varLabel, "at Ignition Locations"), cex.main=1.4)
-  
-  legend_human <- paste("Human, n=", sum(humanMask))
-  legend_lightning <- paste0("Lightning, n=", sum(!humanMask))
-  
-  legend("topleft",
-         legend=c(legend_human, legend_lightning),
-         col=c("orange", "darkgray"),
-         lty=1,
-         cex=1,
-         lwd=4,
-         bty="n")
-
-
-}
 
 quartz(width=15, height=5)
 
@@ -333,9 +369,9 @@ plot_ignition_weather_distribution("t2m", "Temperature [K]", humanMask)
 # Show the locations of the fires
 # TODO: Dot size is size of fire!
 map("state", xlim=c(-125,-102), ylim=c(33,50))
-points(FPA_FOD_subset$LONGITUDE[humanMask], FPA_FOD_subset$LATITUDE[humanMask], 
+points(FPA_FOD$LONGITUDE[humanMask], FPA_FOD$LATITUDE[humanMask], 
        col="orange", pch=".")
-points(FPA_FOD_subset$LONGITUDE[!humanMask], FPA_FOD_subset$LATITUDE[!humanMask], 
+points(FPA_FOD$LONGITUDE[!humanMask], FPA_FOD$LATITUDE[!humanMask], 
        col="darkgray", pch=".")
 title(paste("Fires locations"), cex.main=2)
 
@@ -344,14 +380,14 @@ title(paste("Fires locations"), cex.main=2)
 minDay <- min(FPA_FOD_subset$DISCOVERY_DOY)
 maxDay <- max(FPA_FOD_subset$DISCOVERY_DOY)
 
-hist(FPA_FOD_subset$DISCOVERY_DOY[humanMask], 
+hist(FPA_FOD$DISCOVERY_DOY[humanMask], 
      col=adjustcolor("orange",0.5),
      breaks=c(minDay:maxDay), las=1, xlab="Day of Year",
      ylim=c(0,30),
      border="transparent",
      main="Day of year ignition counts",
      cex.main=2)
-hist(FPA_FOD_subset$DISCOVERY_DOY[!humanMask], 
+hist(FPA_FOD$DISCOVERY_DOY[!humanMask], 
      col=adjustcolor("darkgray", 0.5), breaks=c(minDay:maxDay), add=T,
      border="transparent")
 dev.off()
