7b17667869e78e7b9943f5f00b6d2ca604316f88
Steven <stevenjoelbrey@gmail.com>
2018-04-13 10:41:52 -0600

    Added the ability to plot the SE and W together in a single nice looking row.

--- R/plot_BA_met_r_values.R
+++ R/plot_BA_met_r_values.R
@@ -1,13 +1,15 @@
 # plot_BA_met_r_values.R 
 
-# This script makes plots that display the r values of regional burn area
+# This script makes plots that display the r (cor) values of regional burn area
 # correlation with meteorology values. These r values are calculated using
-# R/plot_BA_Met_relationbs.R. These are values are pearson correlation coefs. 
+# R/plot_BA_Met_relationbs.R. These values are pearson correlation coefs. 
+
+# TODO: indicate which correlations are statistically different from zero. 
 
 library(ggplot2)
 library(ggthemes)
 
-regionName <- "west"
+regionName <- "southeast_and_west"
 
 # where the r (coef) values live. 
 dataDir <- "Data/correlations/"
@@ -37,30 +39,53 @@ if(regionName == "west"){
   # Give nice names to ecoregions 
   levels(df$ecoregion) <- c("Southeast Plains", "Ozark", 
                             "Mississippi Alluvial", "Everglades")
+} else {
+  
+  # both southeast and west together requested
+  df_6.2 <- get(load(paste0(dataDir, "ecoregion_6.2.RData")))
+  df_10.1 <- get(load(paste0(dataDir, "ecoregion_10.1.RData")))
+  df_11.1 <- get(load(paste0(dataDir, "ecoregion_11.1.RData")))
+  
+  df_8.3 <- get(load(paste0(dataDir, "ecoregion_8.3.RData")))
+  df_8.4 <- get(load(paste0(dataDir, "ecoregion_8.4.RData")))
+  df_8.5 <- get(load(paste0(dataDir, "ecoregion_8.5.RData")))
+  df_15.4 <- get(load(paste0(dataDir, "ecoregion_15.4.RData")))
+  
+  # combine each together
+  df <- rbind(df_6.2, df_10.1, df_11.1, df_8.3, df_8.4, df_8.5, df_15.4)
+  
+  df$r <- as.numeric(df$r)
+  df$ecoregion <- factor(df$ecoregion, levels=c("6.2", "10.1", "11.1", "8.3", "8.4", "8.5", "15.4"))
+  # Give nice names to ecoregions 
+  levels(df$ecoregion) <- c("Forested Mt.", "High Deserts", "Med. Cal.",
+                            "S.E. Plains", "Ozark", "MS. Alluvial", "Everglades")
+  
+  
 }
 
 rm(r_df)
 
 df$ignitionType <- factor(df$ignitionType, levels=c("Lightning", "Human"))
-df$variable <- factor(df$variable, levels=c("T2M","TP","RH"))
+df$variable     <- factor(df$variable, levels=c("T2M","TP","RH"))
 
 # Make the plot 
 p <- ggplot(df, aes(x=variable, y=r, fill=ignitionType))+
   geom_bar(stat="identity", width=.5, position = "dodge")+
   scale_x_discrete("Meteorology variable",     
-                   labels=c("T","Precip", "RH%"))+
-  facet_wrap(~ecoregion)+ 
+                   labels=c("T","TP", "RH"))+
+  geom_hline(yintercept=0, linetype="dashed", color = "black")+
+  facet_grid(~ecoregion)+ 
   scale_fill_manual(values=c("gray", "orange"))+
   scale_y_continuous("Pearson correlation")+
   guides(fill=guide_legend(title="Ignition"))+
   # geom_text(aes(label = paste(round(r, 2)), # for labeling the bars 
   #               vjust = ifelse(r >= 0, 0, 1)),
   #           fontface = "bold") +
-  theme_tufte(ticks=F, base_size = 18)
+  theme_tufte(ticks=T, base_size = 15)
 
 # Save the plot
 png(filename=paste0("Figures/regional_met_relations/correlation_summary_",
                     regionName,".png"),
-    width=2300, height=1100, res=260)
-p 
+    width=2400, height=1000, res=260)
+print(p) 
 dev.off()
